c09c28f84c06674b45c1ed412330c38ed172cb03
pradeepsuthar <sutharpradeep081@gmail.com>
2020-06-16 10:12:58 +0530

    add dmart clone

---
+++ js/style.js
@@ -0,0 +1,16 @@
+// Carousel
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:false
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
